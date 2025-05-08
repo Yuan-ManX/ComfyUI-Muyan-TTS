2e8d496808d903008139fbe30f9fedc14e312784
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-08 16:21:24 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
+from .nodes import LoadMuyanTTSModel, LoadRefAudio, PromptText, InputText, Generate, SaveMuyanTTSAudio
 
+NODE_CLASS_MAPPINGS = {
+    "LoadMuyanTTSModel": LoadMuyanTTSModel,
+    "LoadRefAudio": LoadRefAudio,
+    "PromptText": PromptText,
+    "InputText": InputText,
+    "Generate": Generate,
+    "SaveMuyanTTSAudio": SaveMuyanTTSAudio,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadMuyanTTSModel": "Load MuyanTTS Model",
+    "LoadRefAudio": "Load Ref Audio",
+    "PromptText": "Prompt Text",
+    "InputText": "Input Text",
+    "Generate": "Generate",
+    "SaveMuyanTTSAudio": "Save MuyanTTS Audio",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
